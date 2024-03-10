9ca08e0baa36951e108e0bb4c36f39f77d612c78
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2024-03-10 07:44:26 +0330

    .

--- website/src/parsers/pkl/pkl-ts.js
+++ website/src/parsers/pkl/pkl-ts.js
@@ -10,23 +10,44 @@ export default {
   displayName: ID,
   version: `${pkg.version}`,
   locationProps: new Set(['start', 'stop']),
-  _ignoredProperties: new Set(['parent']),
+  _ignoredProperties: new Set(['parent', 'source']),
 
   loadParser(callback) {
     require(['@pkl-ts/parser', '@pkl-ts/parser/dist/antlr.cjs'], callback);
   },
 
   parse(parsers, code, options={}) {
-    console.log(parsers)
-    console.log(code)
-    parsers.pklParser()
+    this.symbolicNames = parsers.PklTsParser.symbolicNames;
+    this.ruleNames = parsers.PklTsParser.ruleNames;
+    let stream = parsers.antlr.CharStream.fromString(code)
+    let tree = parsers.pklParser(stream)
+    return tree.replInput();
   },
 
   nodeToRange(node) {
-    return [node.start.tokenIndex, node.stop.tokenIndex];
+    console.log(node)
+    if (node.getSymbol) {
+      let token = node.getSymbol();
+      return [token.start, token.stop + 1]
+    }
+    if (node.start) {
+      return [node.start.start, node.stop.stop + 1]
+    }
+    
   },
 
   getNodeName(node) {
+    if (node.getSymbol && this.symbolicNames) {
+      let token = node.getSymbol();
+      let tokenType = token.type;
+      return tokenType === -1 ? 'EOF' : this.symbolicNames[tokenType];
+    }
+    if (node._type) {
+      return node._type
+    }
+    if (this.ruleNames && node.ruleIndex !== undefined)  {
+      return this.ruleNames[node.ruleIndex];
+    }
     return node.constructor.name;
   }
 
